577b6c610f91eddfee2e73bb7709f1ab6dd87d86
Ole366 <you@example.com>
2022-12-28 06:05:01 +0300

    OOP

---
+++ Task Seminar/src/Program.java
@@ -0,0 +1,19 @@
+package src;
+
+    
+
+    import java.util.Scanner;
+    
+    public class Program {
+      public static void main(String[] args) {
+        FamilyTree familyTree = new FamilyTree();
+        UserMenu menu = new UserMenu();
+    
+        menu.firstFill(familyTree);
+        menu.launchMenu(familyTree);
+    
+      }
+    
+    } 
+
+
